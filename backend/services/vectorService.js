e5ef21c695eeafe1397936300e3cc208e6cb0000
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-05-03 00:46:24 +0530

    vector service integrated

--- backend/services/vectorService.js
+++ backend/services/vectorService.js
@@ -1,1 +1,15 @@
-ECHO is on.
+//mock bussiness knowledge
+
+const docs = [
+    "Our return policy is 7 days.",
+    "We deliver within 3-5 business days.",
+    "you can track your order using order Id",
+];
+
+const getRelevantDocs = (query) => {
+    return docs.filter(doc => 
+        query.toLowerCase().includes("order") || query.toLowerCase().includes("return")
+    );
+};
+
+module.exports = { getRelevantDocs };
