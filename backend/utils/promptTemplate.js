2685895857cc41d9416c6abb4ca1ed157f98a90c
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-05-03 00:51:27 +0530

    prompt template created

--- backend/utils/promptTemplate.js
+++ backend/utils/promptTemplate.js
@@ -1,1 +1,14 @@
-ECHO is on.
+const buildPrompt = (context, userMessage) => {
+    return [
+        {
+            role: "system",
+            content: `you are a helpful customr support assistant. Use this info:\n${context}`,
+        },
+        {
+            role: "user",
+            content: userMessage,
+        },
+    ];
+};
+
+module.exports = { buildPrompt };
