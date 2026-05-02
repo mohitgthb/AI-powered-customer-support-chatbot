fd96eaa9c840bb64b3a4c6d8bf9fdf199b390003
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-05-02 19:31:16 +0530

    chat model created

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -1,1 +1,13 @@
-ECHO is on.
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+    userId: String,
+    messages: [
+        {
+            role: String,
+            content: String,
+        },
+    ],
+});
+
+module.exports = mongoose.model("Chat", chatSchema);
