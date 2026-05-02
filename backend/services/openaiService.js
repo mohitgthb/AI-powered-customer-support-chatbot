e243a47da22483347f53ccba6890718306fa171a
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-05-03 00:40:35 +0530

    openai service integrated

--- backend/services/openaiService.js
+++ backend/services/openaiService.js
@@ -1,1 +1,15 @@
-ECHO is on.
+const { OpenAI } = require("openai");
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const getLLMResponse = async (messages) => {
+    const res = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages,
+    })
+    return res.choices[0].message.content;
+};
+
+module.exports = { getLLMResponse };
